575c16084dbec22d785015f2eaa0d00c8b01a298
Swarnim112006 <143317120+Swarnim112006@users.noreply.github.com>
2023-08-27 23:18:22 +0530

    Update GETSUGATENSHO.py

--- GETSUGATENSHO.py
+++ GETSUGATENSHO.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Getsugatensho",page_icon=":smiling_face_with_sung
 
 st.header("Yo,welcome everyone to Getsuga-tesnsho")
 st.title("*THE GREATEST WARRIOR*") 
-st.write(" There once was a great invincible warrior,he was the best at sword fights and was undefeatable.He travelled the lands searching for a worthy battle, until he stumbled upon the GATES OF SHADOWS.Despite being warned he opened the gates.The demons of the other world were beeyond them,they rushed upon him,tearing his flesh,draining his soul,turning him into a SHADOW.And now for eternity he is doomed to wander the world and fight! The DEMONS he freed!.(BOSS MUSIC STARTS PPLAYING).")
+st.write(" There once was a great invincible warrior,he was the best at sword fights and was undefeatable.He travelled the lands searching for a worthy battle, until he stumbled upon the GATES OF SHADOWS.Despite being warned he opened the gates.The demons of the other world were beyond them,they rushed upon him,tearing his flesh,draining his soul,turning him into a SHADOW.And now for eternity he is doomed to wander the world and fight! The DEMONS he freed!.(BOSS MUSIC STARTS PPLAYING).")
 st.write("want to read more amazing stories like this the click learn more","[Learn More>](https://youtu.be/1-spTV0m8iI?si=V7rHc1HLnJLu7m3w)")
 st.write("---")
 st.subheader("Do visit us again, Bye")
